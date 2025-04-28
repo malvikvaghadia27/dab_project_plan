b8df2a45622139320afc1fed554f358e3e5d2ff7
Malvik Vaghadia <admin@pathfinder-analytics.net>
2025-04-28 22:09:33 +0100

    updated

--- tests/test_citibike_utils.py
+++ tests/test_citibike_utils.py
@@ -31,6 +31,3 @@ def test_get_trip_duration_mins(get_spark):
     # Assert that the differences are as expected
     assert results[0]["trip_duration_mins"] == 10
     assert results[1]["trip_duration_mins"] == 30
-
-def test_fail():
-    assert 1==2
