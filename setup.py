8dfdf5e991ca31385f5aaf66f80ee5bed127a583
Malvik Vaghadia <admin@pathfinder-analytics.net>
2025-04-28 18:42:10 +0100

    updated whl

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project_plan",
-    version="0.0.1",
+    version="0.0.2",
     description="project_description",
     author="Malvik Vaghadia",
     packages=find_packages(where="./src"),
